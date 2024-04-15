c01538ed5746d5a2147c1f8448f3e9206b17c4e4
Khushi Jain <khushijain3579@gmail.com>
2024-04-15 15:32:01 +0530

    first commit

---
+++ meters/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './component/Navbar';
+import Heading from './component/Heading';
+
+function App() {
+  return (
+    <>
+    
+    <Navbar/>
+    <Heading/>
+    </>
+  );
+}
+
+export default App;
---
+++ meters/src/component/Heading.js
@@ -0,0 +1,14 @@
+import React from 'react'
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Sedan:ital@0;1&display=swap')
+</style>
+function Heading() {
+  return (
+    <div class="font">
+        <h2>Welcome to meters</h2>
+    
+    </div>
+  )
+}
+
+export default Heading
